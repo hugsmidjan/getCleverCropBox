8a04c51e55574c0916ae9e99588bd79dbd666ac1
mar <mar.nospam@anomy.net>
2019-10-07 15:51:52 +0000

    test: Add tests for non-integer input values

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # TODO
 
-- Drop integer requirement for focalData properties
 - Consider relaxing input validation when NODE_ENV==='production'
 - Write README file
 - Convert to TypeScript and autogenerate the `.d.ts` file
--- src/getCleverCropBox.tests.js
+++ src/getCleverCropBox.tests.js
@@ -1,13 +1,17 @@
-import o  from 'ospec';
+import o from 'ospec';
 import getCleverCropBox from './getCleverCropBox';
 
-import assertions from './getCleverCropBox-tests.json';
+import coreAssertions from './getCleverCropBox-tests-core.json'; // shared between the JS and Java implementations
+import extraAssertions from './getCleverCropBox-tests-extra.json'; // JS only - more relaxed inputs, etc.
+
+const assert = ({ name, input, expected }, i) => {
+  const { imageSize, cropInfo, focalPoint } = input;
+  o(name + ' -- "' + JSON.stringify(input) + '"', () => {
+    o(getCleverCropBox(imageSize, cropInfo, focalPoint)).deepEquals(expected);
+  });
+};
 
 o.spec('getCleverCropBox()', () => {
-  assertions.forEach(({ name, input, expected }, i) => {
-		const { imageSize, cropInfo, focalPoint } = input;
-    o(name + ' -- "' + JSON.stringify(input) + '"', () => {
-      o(getCleverCropBox(imageSize, cropInfo, focalPoint)).deepEquals(expected);
-    });
-  });
+  coreAssertions.forEach(assert);
+  extraAssertions.forEach(assert);
 });
